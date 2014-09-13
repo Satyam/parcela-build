56a82fce99ac94dbd1b0ae5030239c321d4bf9ac
Satyam <satyam@satyam.com.ar>
2014-09-13 12:47:28 +0200

    Added jekyll and yuidoc to new site.
    including the themes for API Docs.

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 jshint src/**/*.js
-yuidoc -q
+yuidoc -q --themedir ./apitheme
+browserify -r ./src/parcela:parcela -u fake-dom > ./site/assets/parcela.js
+uglifyjs ./site/assets/parcela.js -c drop_debugger,drop_console,warnings=false -m >./site/assets/parcela-min.js
+jekyll build
+
 
 
