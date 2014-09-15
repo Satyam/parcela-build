655b772829523b42ecd763d8ef38efe239f0a885
Satyam <satyam@satyam.com.ar>
2014-09-15 16:57:08 +0200

    Adds the package.json file to the distribution folder

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 jshint src/**/*.js
 yuidoc -q --themedir ./apitheme
-browserify -r ./src/parcela:parcela -u fake-dom > ./site/assets/parcela.js
-uglifyjs ./site/assets/parcela.js -c drop_debugger,drop_console,warnings=false -m >./site/assets/parcela-min.js
+browserify -r ./src/parcela:parcela -u fake-dom > ./site/dist/parcela.js
+uglifyjs ./site/dist/parcela.js -c drop_debugger,drop_console,warnings=false -m >./site/dist/parcela-min.js
+cp ./src/parcela/package.json ./site/dist/
 jekyll build
-
-
-
