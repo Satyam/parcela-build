be9fc04de9d225ee07195f0758849f88b46f4dac
Satyam <satyam@satyam.com.ar>
2014-09-17 15:36:47 +0200

    There is no need to copy the package.json for Parcela

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@ jshint src/**/*.js
 yuidoc -q --themedir ./apitheme
 browserify -r ./src/parcela:parcela -u fake-dom > ./site/dist/parcela.js
 uglifyjs ./site/dist/parcela.js -c drop_debugger,drop_console,warnings=false -m >./site/dist/parcela-min.js
-cp ./src/parcela/package.json ./site/dist/
 jekyll build
