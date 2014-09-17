b903e6ad0e239ca95bce7090b4eab3e8916990c6
Satyam <satyam@satyam.com.ar>
2014-09-17 19:56:29 +0200

    Change the building of the API docs for new repository

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 jshint src/**/*.js
+mkdir ./site/apitmp
 yuidoc -q --themedir ./apitheme
+mv -f -t ./site/api ./site/apitmp/* 
+rmdir ./site/apitmp
 browserify -r ./src/parcela:parcela -u fake-dom -u jsdom > ./site/dist/parcela.js
 uglifyjs ./site/dist/parcela.js -c drop_debugger,drop_console,warnings=false -m >./site/dist/parcela-min.js
 jekyll build
